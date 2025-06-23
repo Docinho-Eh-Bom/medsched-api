dbb504d7b173a299ee7ddd9949883119e44b8c69
DocinhoÉBom <alessandrafreitaspacheco@gmail.com>
2025-06-23 18:02:02 -0300

    Service start and controllers file created

--- src/services/users-service.ts
+++ src/services/users-service.ts
@@ -1,6 +1,33 @@
-import { APIError } from "../errors/api-error";
-import { User, UserRole, PatientData, MedicData } from "../models/user";
-import { UserRepositoryInterface } from "../repositories/user-repository.interface";
-import { hashPassword, comparePassword, generateToken } from "../utils/jwt";
+import { userRoleValidation } from "../schema/users-schema";
+import { UserRepository } from "../repositories/user-repository";
+import { User, UserRole } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { NotFoundError } from "../errors/not-found-error";
+import { ConflictError } from "../errors/conflict-error";
+import { UnauthorizedError } from "../errors/unauthorized-error";
+import { hashPassword, comparePassword, generateToken  } from "../utils/jwt";
+import { z } from "zod";
+
+type CreateUserData = z.infer<typeof userRoleValidation.createUserSchema>;
+type UpdateUserData = z.infer<typeof userRoleValidation.updateUserSchema>;
+
+export interface AuthResponse{
+    user: Omit<User, 'password'>;
+    token: string;
+}
+
+export class UserService{
+    private userRepository: UserRepository;
+
+    constructor() {
+        this.userRepository = new UserRepository();
+    }
+
+    async createUser(data: CreateUserData)
+
+
+
+}
+
 
 
