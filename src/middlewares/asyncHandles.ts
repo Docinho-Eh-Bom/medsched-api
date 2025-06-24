1b099479269ba012c1bde23a023eeb99255d0cab
DocinhoÉBom <alessandrafreitaspacheco@gmail.com>
2025-06-24 17:15:40 -0300

    asynchandle update

--- src/middlewares/asyncHandles.ts
+++ src/middlewares/asyncHandles.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
+//middleware to handle async errors because i was getting so much errors on my routes
 export const asyncHandler = (
     fn: (req: Request, res: Response, next: NextFunction) => Promise<void> ) => {
         return (req: Request, res: Response, next: NextFunction) => {
