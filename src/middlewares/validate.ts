c4c3ad1b0e655fa19b7b1c5b0af75ec2b6be5f99
DocinhoÉBom <alessandrafreitaspacheco@gmail.com>
2025-06-23 23:16:58 -0300

    zod validation middleware created

---
+++ src/middlewares/validate.ts
@@ -0,0 +1,24 @@
+import { ZodSchema } from "zod";
+import { Request, Response, NextFunction } from "express";
+
+type ValidateSchemas = {
+    body?: ZodSchema;
+    query?: ZodSchema;
+    params?: ZodSchema; 
+};
+
+export const validate = (schemas: ValidateSchemas) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try{
+            if(schemas.body) req.body = schemas.body.parse(req.body);
+            if(schemas.query) req.query = schemas.query.parse(req.query);
+            if(schemas.params) req.params = schemas.params.parse(req.params);
+            next();
+        }catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ error: error.message });
+            }
+            return res.status(500).json({ error: "Internal server error" });
+        }
+    };
+};
