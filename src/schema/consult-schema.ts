791bdb20f9d2dad5d1f08195576f614b8e9df9c6
DocinhoÉBom <alessandrafreitaspacheco@gmail.com>
2025-06-26 17:00:44 -0300

    schema update

--- src/schema/consult-schema.ts
+++ src/schema/consult-schema.ts
@@ -19,10 +19,17 @@ export const CreateConsultSchema = ConsultSchema.omit({
     id: true, 
     createdAt: true, 
     updatedAt: true }).extend({
-        date: z.string().datetime().transform((val) => new Date(val)),
+        date: z.preprocess((val) => typeof val === "string" ? new Date(val) : val,z.date().min(new Date(), "Consult date cannot be in the past")),
+        status: z.enum(['SCHEDULED', 'COMPLETED', 'CANCELLED']).default('SCHEDULED').optional(),
         notes: z.string().max(1000, "Notes must be less than 1000 characters").optional(),
     });
 
 export const UpdateConsultStatusSchema = z.object({
     status: ConsultStatusSchema,
 });
+
+//schema for notes
+export const UpdateConsultNotesSchema = z.object({
+    notes: z.string().max(1000, "Notes must be less than 1000 characters")
+});
+
