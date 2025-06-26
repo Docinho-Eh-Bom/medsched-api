6391425c3ec63825f66c4848232dcd6eee50749d
DocinhoÉBom <alessandrafreitaspacheco@gmail.com>
2025-06-26 17:00:31 -0300

    routes update

--- src/routes/consult-routes.ts
+++ src/routes/consult-routes.ts
@@ -3,64 +3,71 @@ import { asyncHandler } from "../middlewares/asyncHandles";
 import { ConsultController } from "../controllers/consult-controller";
 import { authenticate, authorize } from "../middlewares/auth";
 import { validate } from "../middlewares/validate";
-import { CreateConsultSchema, UpdateConsultStatusSchema } from "../schema/consult-schema";
+import { CreateConsultSchema, UpdateConsultStatusSchema, UpdateConsultNotesSchema } from "../schema/consult-schema";
 
 const router = Router();
 const controller = new ConsultController();
 
 //create consult
-router.post("/consults", authenticate,
+router.post("/", authenticate,
     authorize("patient", "admin"),
     validate({body: CreateConsultSchema}),
     asyncHandler(controller.createConsult.bind(controller))
 );
 
 //get all consults
-router.get("/consults", authenticate,
+router.get("/", authenticate,
     authorize("admin"),
     asyncHandler(controller.listAll.bind(controller))
 );
 
 //list consults by status
-router.get("/consults/status/:status", authenticate,
+router.get("/status/:status", authenticate,
     authorize("medic", "admin"),
     asyncHandler(controller.listByStatus.bind(controller))
 );
 
 //get consult by id
-router.get("/consults/:id", authenticate,
+router.get("/:id", authenticate,
     authorize("patient", "medic", "admin"),
     asyncHandler(controller.getById.bind(controller))
 );
 
 //get consults by patient id
-router.get("/consults/patient/:patientId", authenticate,
+router.get("/patient/:patientId", authenticate,
     authorize("patient", "medic", "admin"),
     asyncHandler(controller.getByPatientId.bind(controller))
 );
 
 //get consults by medic id
-router.get("/consults/medic/:medicId", authenticate,
+router.get("/medic/:medicId", authenticate,
     authorize("medic", "admin"),
     asyncHandler(controller.getByMedicId.bind(controller))
 );
 
 //update consult status
-router.patch("/consults/:id/status", authenticate,
+router.patch("/:id/status", authenticate,
     authorize("medic", "admin"),
     validate({body: UpdateConsultStatusSchema}),
     asyncHandler(controller.updateConsultStatus.bind(controller))
 );
 
-//add notes to the consult
-router.patch("/consults/:id/notes", authenticate,
-    authorize("medic", "admin"),
+//cancel consult
+router.patch("/:id/cancel", authenticate,
+    authorize("patient", "medic", "admin"),
     validate({body: UpdateConsultStatusSchema}),
+    asyncHandler(controller.updateConsultStatus.bind(controller))
+);
+
+//add notes to the consult
+router.patch("/:id/notes", authenticate,
+    authorize("medic", "admin"),
+    validate({body: UpdateConsultNotesSchema}),
     asyncHandler(controller.addNotes.bind(controller))
 );
 
 //delete consult
-router.delete("/consults/:id", authenticate,
+router.delete("/:id", authenticate,
     authorize("admin"),
     asyncHandler(controller.deleteConsult.bind(controller))
 );
--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -3,33 +3,46 @@ import { Router } from "express";
 import { UsersController } from "../controllers/users-controller";
 import { validate } from "../middlewares/validate";
 import { authenticate, authorize } from "../middlewares/auth";
+import { registerSchema } from "../schema/auth-schema";
 import {  userRoleValidation } from "../schema/users-schema";
 
 const router = Router();
 const controller = new UsersController();
 
 //list all users(admins only)
-router.get("/users",authenticate, 
+router.get("/",authenticate, 
     authorize('admin'),
     asyncHandler(controller.listAll.bind(controller)));
 
 //get by id(admin or user of that id)
 router.get("/:id", authenticate,
+    authorize('admin', 'patient'),
     asyncHandler(controller.getUserById.bind(controller)));
+
+//add medic slot
+router.post("/:userId/slots", authenticate,
+    authorize('medic', 'admin'),
+    validate({ body: userRoleValidation.addMedicSlot }),
+    asyncHandler(controller.addMedicSlot.bind(controller)));
+
+//get medic slots
+router.get("/:userId/slots", authenticate,
+    authorize('medic','patient', 'admin'),
+    asyncHandler(controller.getMedicSlots.bind(controller)));
     
 //create user (admin only)
 router.post("/users", authenticate,
     authorize('admin'),
-    validate({ body: userRoleValidation.createUserSchema }),
+    validate({ body: registerSchema }),
     asyncHandler(controller.createUser.bind(controller)));
 
 //update user (admin or user of that id)
-router.put("/:id", authenticate,
+router.patch("/:userId", authenticate,
     validate({ body: userRoleValidation.updateUserSchema }),
     asyncHandler(controller.updateUser.bind(controller)));
 
 //delete user (admin or user of that id)
-router.delete("/:id", authenticate,
+router.delete("/:userId", authenticate,
     authorize('admin', 'patient', 'medic'),
     asyncHandler(controller.deleteUser.bind(controller)));
 
