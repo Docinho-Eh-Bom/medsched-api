f8669a7b40ecf5821b4fe445e41e5e711dafe791
DocinhoÉBom <alessandrafreitaspacheco@gmail.com>
2025-06-26 23:08:45 -0300

    api-error update

--- src/errors/api-error.ts
+++ src/errors/api-error.ts
@@ -1,9 +1,13 @@
 export class APIError extends Error {
     public statusCode: number;
+    public errors?: any;
 
-    constructor(message: string, statusCode: number){
+    constructor(message: string, statusCode: number, errors?: any) {
         super(message);
-        this.statusCode = statusCode;
+        this.statusCode = 400;
+        this.errors = errors;
         this.name = "APIError";
+        Object.setPrototypeOf(this, new.target.prototype);
+        Error.captureStackTrace(this);
     }
 }
